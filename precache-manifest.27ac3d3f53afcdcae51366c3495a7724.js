acbd1d5466f55145ba2b3f2b13491761395ecd31
preeti0388 <preeti0388@gmail.com>
2019-03-16 23:31:00 +1100

    Updates

---
+++ precache-manifest.27ac3d3f53afcdcae51366c3495a7724.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4ae4cf7face221903b8f",
+    "url": "/pickplatterapp/static/js/runtime~main.bb6fa0a2.js"
+  },
+  {
+    "revision": "9ab7e3b7c4376eb07046",
+    "url": "/pickplatterapp/static/js/main.497fb0e1.chunk.js"
+  },
+  {
+    "revision": "5d21101413ea9baee411",
+    "url": "/pickplatterapp/static/js/2.589c286c.chunk.js"
+  },
+  {
+    "revision": "9ab7e3b7c4376eb07046",
+    "url": "/pickplatterapp/static/css/main.9a55c28b.chunk.css"
+  },
+  {
+    "revision": "813416837d4b88ce2bc28329d18b32fb",
+    "url": "/pickplatterapp/index.html"
+  }
+];
